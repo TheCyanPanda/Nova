f36f54563a83365ee4a33540c01d076458c55648
Christoffer Ljung <christoffer@ljung.io>
2022-04-03 19:04:41 +0100

    Added test script for TCP server
    Also renamed the udp script

---
+++ py_scripts/tcp_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import socket
+import time
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 3333  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    
+    t0 = time.time()
+    t1 = 0
+    while t1 < 20:  # Run for X seconds
+        s.sendall(b'Testing testing\n')
+        time.sleep(1)
+        t1 = time.time() - t0
+        data = s.recv(1024)
+        print(f"Received {data!r}")
+
+print(f"Received {data!r}")
