f00e17737df579417e03a4f3bacad7b77c32a643
Christoffer Ljung <christoffer@ljung.io>
2022-04-03 10:26:48 +0100

    Remove build files

--- build/objects/src/socket/PracticalSocket..d
+++
@@ -1,2 +0,0 @@
-build/objects/src/socket/PracticalSocket..o: \
- src/socket/PracticalSocket..cpp include/socket/PracticalSocket.hpp
--- build/objects/src/socket/mod1c2.d
+++
@@ -1,2 +0,0 @@
-build/objects/src/socket/mod1c2.o: src/socket/mod1c2.cpp \
- include/socket/mod1c2.hpp
