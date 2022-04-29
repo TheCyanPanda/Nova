ff027f40e153de3cd137fa706dffb2e5c0c30804
Christoffer Ljung <christoffer@ljung.io>
2022-04-29 19:12:37 +0100

    Control camera servo over TCP

--- include/IOHandler/ioHandler.h
+++ include/IOHandler/ioHandler.h
@@ -9,24 +9,32 @@
 class ioHandler
 {
 private:
-    enum class Commands {CAMERA_MOVE};
-
-    Network::CameraStream* cameraStream;
+    // Variables
+    enum class cameraPosition {MID, MIN, MAX};
+    std::vector<Network::CameraStream*> cameraStreams;
     Network::TCPServer* tcpServer;
     GPIO::ServoInterface* cameraServo;
 
+    std::map<std::string, cameraPosition> cameraPositions;
     std::map<std::string, std::function<void(const std::string&)>> functionMap;
-    void fnMoveCamera(const std::string& v_cmd) const;
 
+        // Init functions
     void initTcpServerCallbacks();
     void initValidCommands();
+
+    // Callable over TCP
+    void fnMoveCamera(const std::string& v_cmd) const;
+    void fnStartUdpStream(const std::string v_cmd);
+
+    // Functions
     int parseTcpMessage(const std::string& msg, const std::string& username);
     void startTcpServer();
-    void startUdpServer();
 
 public:
     ioHandler();
     ~ioHandler();
+
+    const std::vector<std::string> userInput2Vec(const std::string& input, const char* separator=" ") const;
 };
 
 #endif /* IO_HANDLER_H */
--- src/IOHandler/ioHandler.cpp
+++ src/IOHandler/ioHandler.cpp
@@ -6,21 +6,16 @@
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/predicate.hpp>
 
+
+                                            /* ---------- CONSTRUCTOR / DESTRUCTOR  ----------  */
 // Constructor
 ioHandler::ioHandler()
 {
-    this->cameraStream = new Network::CameraStream("192.168.99.12", "39009"); // Init with arbitrary address for now. TODO: Start the stream on demand with request from specific IP. TODO: Let port be an int instaed of std::str
     this->tcpServer = new Network::TCPServer(Network::IPV::V4, 1234);
     this->cameraServo = new GPIO::ServoInterface(18);
 
     this->initValidCommands();
     this->initTcpServerCallbacks();
-
-    // UDP stream start
-    // this->startUdpServer();
-
-    // // Test Servo
-    this->cameraServo->moveMid();
 
     // // Start TCP Server
     this->startTcpServer();
@@ -29,10 +24,13 @@ ioHandler::ioHandler()
 // Destructor
 ioHandler::~ioHandler()
 {
-    delete this->cameraStream;
+    for (size_t i = 0; i < this->cameraStreams.size(); i++) {
+        delete this->cameraStreams[i];
+    }
     delete this->tcpServer;
     delete this->cameraServo;
 }
+                                            /* ---------- PRIVATE FUNCTIONS ----------  */
 
 void ioHandler::initTcpServerCallbacks()
 {
@@ -54,10 +52,10 @@ void ioHandler::initTcpServerCallbacks()
         // Parse message
         int ret = this->parseTcpMessage(message, client->getUsername());
         if (ret) {
-            std::cout << "Error..." << "\n"; // todo: fix.
+            std::cout << "Error : Received invalid command." << "\n"; // todo: fix. Do something useful if invalid command was received.
         }
 
-        // Send response to all connected clients
+        // TODO: Log the received command:
         std::stringstream ss;
         ss << client->getUserId() << " : " << client->getUsername() << " Sent message: " << message;
         //this->tcpServer->broadcast(ss.str()); // todo: don't do this!
@@ -74,43 +72,84 @@ void ioHandler::initTcpServerCallbacks()
 
 void ioHandler::initValidCommands()
 {
+    // TODO: Log all commands registered.
+    // TODO: Create command to register user. Only registered users can move the camera, start the UDP server etc. Make different auth levels.
+
+
+    /* ------ Init cameraPosition ------ */
+    // Function call init
     this->functionMap["cam_move"] = [this](const std::string& v_cmd) {
-        // TODO: Log entry etc
         this->fnMoveCamera(v_cmd);
     };
+    // Init cam pos map
+    this->cameraPositions["MID"] = cameraPosition::MID;
+    this->cameraPositions["MIN"] = cameraPosition::MIN;
+    this->cameraPositions["MAX"] = cameraPosition::MAX;
 
+    /* ------ Init UDP Stream ------ */
+    this->functionMap["udp_start"] = [this](const std::string& v_cmd) {
+        this->fnStartUdpStream(v_cmd);
+    };
 }
 
 
 void ioHandler::fnMoveCamera(const std::string& v_cmd) const
 {
-    
-    std::vector argv = Common::stringSplit(v_cmd, " ");
-    enum position {MID_POS, MIN_POS, MAX_POS};
-    static std::map<std::string, position> string_map;
-    string_map["mid"] = MID_POS;
-    string_map["min"] = MIN_POS;
-    string_map["max"] = MAX_POS;
+    /* Usage: cam_move <MIN | MID | MAX > */
+    std::vector<std::string> argv = this->userInput2Vec(v_cmd, " ");
 
-    switch(string_map[argv.at(1).c_str()]) { // TODO: Find a way to make this work.
-        case MID_POS:
-            std::cout << "moving camera to mid" << "\n";
+    if (this->cameraPositions.find(argv.at(1)) == this->cameraPositions.end()) {
+        std::cout << "Error: " << "Trying to move camera to invalid location: " << argv.at(1) << "\n"; // todo: Log this.
+        return;
+    }
+
+    switch(this->cameraPositions.at(argv.at(1).c_str())) { // TODO: Log the entries
+        case cameraPosition::MID:
+            std::cout << "moving camera to MID" << "\n";
+            this->cameraServo->moveMid();
             break;
-        case MIN_POS:
+        case cameraPosition::MIN:
             std::cout << "moving camera to MIN" << "\n";
+            this->cameraServo->moveMin();
             break;
-        case MAX_POS:
+        case cameraPosition::MAX:
             std::cout << "moving camera to MAX" << "\n";
+            this->cameraServo->moveMax();
             break;
         default:
             std::cout << "WARNING: Invalid option..." << "\n";
     }
-    this->cameraServo->moveMid();
+}
 
+void ioHandler::fnStartUdpStream(const std::string v_cmd)
+{
+    /* Usage: udp_start <ip> <port> */
+    //this->cameraStreams.push_back(new Network::CameraStream(target_ip, target_port));
+    //this->cameraStream = new Network::CameraStream("192.168.99.12", "39009"); // Init with arbitrary address for now. TODO: Start the stream on demand with request from specific IP. TODO: Let port be an int instaed of std::str
+    //this->cameraStream->startStream();
+
+    std::vector<std::string> argv = this->userInput2Vec(v_cmd, " ");
+
+    if (argv.size() != 3) {
+        std::cout << "Client attempted to run command 'start_udp' with invalid number of arguments: " << argv.size() << "\n"; // todo: save in log
+    }
+    std::string target_ip = argv.at(1);
+    std::string target_port = argv.at(2);
+
+    // TODO: Associate the server with a userIP/ID. Kill and delete the UDP socket after X minutes/hours etc
+    // if there is no activity from the user. Require an interatcion "I am alive" etc every X minutes to keep the server alive.
+    this->cameraStreams.push_back(new Network::CameraStream(target_ip, target_port)); 
+}
+
+
+void ioHandler::startTcpServer()
+{
+    this->tcpServer->run();
 }
 
 int ioHandler::parseTcpMessage(const std::string& msg, const std::string& username)
 {
+
     std::vector<std::string> res = Common::stringSplit(msg, " ");
 
     // Validate sender as actual client and pop it from vector
@@ -120,6 +159,8 @@ int ioHandler::parseTcpMessage(const std::string& msg, const std::string& userna
     } else {
         return -1;
     }
+    // TODO: Require each command to pass a simple authentication step like: send(vcmfd9U@#nvcv2~!fd23g). Expect a returned hash to match from the client.
+    // TODO: Check if user is registered. Let only registered users continue
 
     auto check_fn = this->functionMap.find(res.front());
     if (check_fn != this->functionMap.end()) {
@@ -132,18 +173,19 @@ int ioHandler::parseTcpMessage(const std::string& msg, const std::string& userna
         return -1; // Command not found among registered in functionMap (see fn initValidCommands)
     }
 
-    // std::cout << "Message received : " << msg << "\n"; // TODO: Store in log with timestamp
-
     return 0;
 }
 
 
-void ioHandler::startTcpServer()
-{
-    this->tcpServer->run();
-}
+                                            /* ---------- PUBLIC FUNCTIONS ----------  */
 
-void ioHandler::startUdpServer()
-{
-    this->cameraStream->startStream();
-}
+ const std::vector<std::string> ioHandler::userInput2Vec(const std::string& input, const char* separator) const
+ {
+    std::vector<std::string> argv;
+    boost::split(argv, input, algorithm::is_any_of(separator));
+    for (auto& i : argv) { // Remove whitespace for each argument
+        algorithm::trim(i);
+    }
+
+    return argv;
+ }
